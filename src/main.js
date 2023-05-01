fdc232e055dadc4e29df876f59dc18078d83184c
HOME <homegor14@gmail.com>
2023-05-01 21:18:58 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,47 +1,37 @@
 import { Telegraf, session } from 'telegraf'
-import { message } from "telegraf/filters";
-import { code } from "telegraf/format";
+import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format'
 import config from 'config'
 import { ogg } from './ogg.js'
-import { openai } from "./openai.js"
-
-const INITIAL_SESSION = {
-    message: []
-}
+import { openai } from './openai.js'
+import { removeFile } from './utils.js'
+import { initCommand, processTextToChat, INITIAL_SESSION } from './logic.js'
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
 bot.use(session())
 
-bot.command('new', async (ctx) => {
-    ctx.session = INITIAL_SESSION
-    await ctx.reply('Жду вашего голосового или текстового сообщения')
-})
-bot.command('start', async (ctx) => {
-    ctx.session = INITIAL_SESSION
-    await ctx.reply('Жду вашего голосового или текстового сообщения')
-})
+bot.command('new', initCommand)
 
-bot.on(message('voice'), async  (ctx) => {
+bot.command('start', initCommand)
+
+bot.on(message('voice'), async (ctx) => {
     ctx.session ??= INITIAL_SESSION
-    try{
-        await ctx.reply(code('Ждем ответа'))
+    try {
+        await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
         const userId = String(ctx.message.from.id)
         const oggPath = await ogg.create(link.href, userId)
         const mp3Path = await ogg.toMp3(oggPath, userId)
 
+        removeFile(oggPath)
+
         const text = await openai.transcription(mp3Path)
         await ctx.reply(code(`Ваш запрос: ${text}`))
 
-        ctx.session.message.push({role: openai.roles.USER, content: text })
-        const response = await openai.chat(ctx.session.message)
-
-        ctx.session.message.push({role: openai.roles.ASSISTANT, content: response.content })
-
-        await ctx.reply(response.content)
-    } catch (e){
-        console.log('error while voice message', e.message)
+        await processTextToChat(ctx, text)
+    } catch (e) {
+        console.log(`Error while voice message`, e.message)
     }
 })
 
@@ -49,22 +39,9 @@ bot.on(message('text'), async (ctx) => {
     ctx.session ??= INITIAL_SESSION
     try {
         await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
-
-        ctx.session.message.push({
-            role: openai.roles.USER,
-            content: ctx.message.text
-        })
-
-        const response = await openai.chat(ctx.session.message)
-
-        ctx.session.message.push({
-            role: openai.roles.ASSISTANT,
-            content: response.content
-        })
-
-        await ctx.reply(response.content)
+        await processTextToChat(ctx, ctx.message.text)
     } catch (e) {
-        console.log(`Error while text message`, e.message)
+        console.log(`Error while voice message`, e.message)
     }
 })
 
